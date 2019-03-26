2359d28e8661c46ce66c7d14f57e267db45f3f86
jgalvelo <jose.alvelo@cunisanjuan.edu>
2019-03-26 18:26:03 -0400

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CarService } from "./car.service";
+import { Car } from "./car";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'PrimeCars';
+export class AppComponent implements OnInit {
+  displayDialog: boolean;
+
+  car: Car = {};
+
+  selectedCar: Car;
+
+  newCar: boolean;
+
+  cars: Car[];
+
+  cols: any[];
+
+  constructor(private carService: CarService) {}
+
+  ngOnInit() {
+    this.carService.getCarsSmall().subscribe((res: any[]) => {
+      this.cars = res.data;
+    });
+
+    this.cols = [
+      { field: "vin", header: "Vin" },
+      { field: "year", header: "Year" },
+      { field: "brand", header: "Brand" },
+      { field: "color", header: "Color" }
+    ];
+  }
+
+  showDialogToAdd() {
+    this.newCar = true;
+    this.car = {};
+    this.displayDialog = true;
+  }
+
+  save() {
+    let cars = [...this.cars];
+    if (this.newCar) {
+      cars.push(this.car);
+    } else {
+      cars[this.cars.indexOf(this.selectedCar)] = this.car;
+    }
+
+    this.cars = cars;
+    this.car = null;
+    this.displayDialog = false;
+  }
+
+  delete() {
+    let index = this.cars.indexOf(this.selectedCar);
+    this.cars = this.cars.filter((val, i) => i != index);
+    this.car = null;
+    this.displayDialog = false;
+  }
+
+  onRowSelect(event) {
+    this.newCar = false;
+    this.car = this.cloneCar(event.data);
+    this.displayDialog = true;
+  }
+
+  cloneCar(c: Car): Car {
+    let car = {};
+    for (let prop in c) {
+      car[prop] = c[prop];
+    }
+    return car;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {TableModule} from 'primeng/table';
+import {InputTextModule} from 'primeng/inputtext';
+import {DialogModule} from 'primeng/dialog';
+import { CarService } from "./car.service";
+import {DropdownModule} from 'primeng/dropdown';
+import { ScrollDispatcher } from '@angular/cdk/scrolling';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
+import {ButtonModule} from 'primeng/button';
+
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    TableModule,
+    DialogModule,
+    DropdownModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ButtonModule
   ],
-  providers: [],
+  providers: [
+    CarService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
